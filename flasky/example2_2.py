1155b06039fb031cb6d8ba4523970e48a0d3a32a
Pxli9130 <peixuanli0319@gmail.com>
2024-09-24 10:54:09 -0400

    finish example 2.2 , now use dynamic route

---
+++ flasky/example2_2.py
@@ -0,0 +1,11 @@
+# example 2.1
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+# example 2.2
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
