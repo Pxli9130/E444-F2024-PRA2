99e792fcd2bb4e91c4ad2d128f39c26500f10cbe
Pxli9130 <peixuanli0319@gmail.com>
2024-09-25 03:24:39 -0400

    add time use flask moment

--- flasky/example3.py
+++ flasky/example3.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -14,8 +17,9 @@ def internal_server_error(e):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name='Peixuan')
+
